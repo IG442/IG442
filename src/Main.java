7cc2e4a028b70c2cd3919d478010b2a083dac2c3
IG442 <mrazrael1991@gmail.com>
2025-09-20 17:03:02 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class printThreeWords {
+    public static void main(String[] args) {
+
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
